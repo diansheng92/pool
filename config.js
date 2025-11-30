2d51b22e6f7ab9d4bc4e9a14b6ca2b8e3320818b
shengdiansd <shengdiansd@gmail.com>
2025-11-29 21:57:03 -0500

    chore: update API URL to pool.azurewebsites.net

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 // Global configuration for API base URL.
-// Replace <WEBAPP_NAME> with your actual Azure Web App name after deployment.
-// Example: https://pool-web-app-12345.azurewebsites.net
-window.API_URL = 'https://<WEBAPP_NAME>.azurewebsites.net';
+// Your Azure Web App URL
+window.API_URL = 'https://pool.azurewebsites.net';
